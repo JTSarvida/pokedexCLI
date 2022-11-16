5e67fe3e64583b14370068cbcd50da0c802ebbe8
jtsarvida <jtsarvida@gmail.com>
2022-11-16 01:06:36 -0800

    Updated the ReadMe to give a brief summary of this gem.

---
+++ spec/pokedex_cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PokedexCli do
+  it "has a version number" do
+    expect(PokedexCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
