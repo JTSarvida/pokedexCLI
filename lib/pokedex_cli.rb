b1303f1b25a52bfca90acfd00a662e346c72fee2
jtsarvida <jtsarvida@gmail.com>
2022-11-17 03:06:29 -0800

    Added require_relative in our environment file in order to make it require the CLI file.

--- lib/pokedex_cli.rb
+++ lib/pokedex_cli.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "pokedex_cli/version"
+require_relative "pokedex_cli/CLI.rb"
 
 module PokedexCli
   class Error < StandardError; end
--- lib/pokedex_cli/CLI.rb
+++ lib/pokedex_cli/CLI.rb
@@ -1,3 +1,8 @@
 class PokedexCli::CLI
-    
+
+    def call
+        puts 'Hello! I am Dexter, a Pokedex created by Professor Josh in order to help you with any Pokemon needs.'
+    end
+
+
 end
